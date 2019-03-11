0560b15e6e87a352ed810b196bc9b3abee602408
elianocosta <eliano.costa@edu.unifor.br>
2019-03-11 10:05:01 -0300

    'adicionando'

---
+++ app/src/main/java/aula2/dominio/com/br/rgba_calculator50/MainActivity.kt
@@ -0,0 +1,12 @@
+package aula2.dominio.com.br.rgba_calculator50
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
